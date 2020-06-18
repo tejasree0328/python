445252d396629c7a8b1343d85b46c01235d2ca00
tejasree0328 <67118578+tejasree0328@users.noreply.github.com>
2020-06-18 23:15:51 +0530

    Add files via upload

---
+++ python_assignment_1.py
@@ -0,0 +1,93 @@
+python_assignment_1
+
+# Teja sree T - 321810306038
+
+
+# to design Simple Calculator
+x,y=10,10      #Addition
+print(x+y)
+
+x=15
+y=15
+z=x+y
+print(z)
+
+x,y=20,10          #Subtraction
+print(x-y)
+
+x,y=5,3               #Multiplication
+print(x*y)
+
+x,y=60,30           #Division
+print(x/y)
+
+x,y=100,15         #Modulus
+print(x%y)
+
+x=4**4                #Exponentiation
+print(x)
+
+х,у=60,30            #Floor Division
+print(x//y)
+
+ 
+# Simple Interest(si)
+p=150
+t=14
+r=3
+si=p*t*r/100
+print(si)
+
+
+# Area of a Circle
+PI=3.14
+r=5
+Area=PI*r*r
+print(Area)
+
+
+# Area of Triangle
+a=10
+b=4
+Area=(a*b/2)
+print(Area)
+
+
+# Temperature in Celsius to Fahrenheit
+celsius=22.5
+fahrenheit=(celsius*1.8)+32
+print(fahrenheit)
+
+
+# Area of Rectangle
+w=5
+I=8
+Area=w*
+print(Area)
+
+
+# Perimeter of a Square
+a=20
+P=4*a
+print(P)
+
+
+# Circumference of a Circle
+PI=3.14
+r=4
+C=2*PI*r
+print(c)
+
+
+# Swapping of Two numbers
+x,y=10,20
+temp=x
+x=y
+y=temp
+print(x,y)
+
+x=input('Enter value of x:')
+y=input('Enter value of y:')
+x,y=y,x
+print(x)
+print(y)
